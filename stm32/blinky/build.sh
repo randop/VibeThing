5bd8ce161cc9e605944fa76b10dec061edcc0b57
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-01-02 12:52:35 +0000

    Include a build script

---
+++ stm32/blinky/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -f blinky.bin &&
+  rm -f stm32*.elf &&
+  make &&
+  arm-none-eabi-objcopy -O binary stm32-blinky-f1.elf blinky.bin &&
+  st-flash --reset write blinky.bin 0x08000000
